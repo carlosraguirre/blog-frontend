8f1101d253fbccc124c4b0ff764dbd4067495bbf
Carlos Aguirre <craguirre952@gmail.com>
2021-07-27 22:21:25 -0400

    add navbar to App.vue and add isLoggedIn

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,37 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/posts">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/signup">Signup</router-link> |
-      <router-link to="/login">Login</router-link> |
-      <router-link to="/logout">Logout</router-link> |
-      <router-link to="/posts/new">New Posts</router-link>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">Bob Loblaw's Law Blog</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/posts">Home</a>
+            </li>
+            <li class="nav-item" v-if="!isLoggedIn()">
+              <a class="nav-link" href="/signup">Signup</a>
+            </li>
+            <li class="nav-item" v-if="!isLoggedIn()">
+              <a class="nav-link" href="/login">Login</a>
+            </li>
+            <li class="nav-item" v-if="isLoggedIn()">
+              <a class="nav-link" href="/logout">Logout</a>
+            </li>
+            <li class="nav-item" v-if="isLoggedIn()">
+              <a class="nav-link" href="/posts/new">New Post</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <p>logged in? {{ isLoggedIn() }}</p>
+    <p>user id? {{ getUserId() }}</p>
+    <div class="container">
+      <router-view/>
     </div>
-    <router-view/>
   </div>
 </template>
 
@@ -34,3 +57,20 @@
   color: #42b983;
 }
 </style>
+
+<script>
+export default {
+  methods: {
+    isLoggedIn: function() {
+      if (localStorage.getItem("jwt")) {
+        return true;
+      } else {
+        return false;
+      }      
+    },
+    getUserId: function() {
+      return localStorage.getItem("user_id");
+    }
+  }
+}
+</script>
